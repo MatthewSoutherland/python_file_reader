f620fffafa084c6757dda535a04d9edf4f1e4064
Matthew Guy Southerland <mgsoutherland529@gmail.com>
2023-01-02 10:42:50 -0500

    feat: added python csv work

---
+++ csv_read_write.py
@@ -0,0 +1,13 @@
+import csv
+
+# import csv
+with open("one.csv", "r") as f:
+    file_reader = csv.DictReader(f)
+
+    with open("new.csv", "w", newline="") as w:
+        fieldnames = ["first", "second"]
+        file_writer = csv.DictWriter(w, fieldnames=fieldnames)
+        file_writer.writeheader()
+
+        for line in file_reader:
+            file_writer.writerow(line)
